9a4ccab2a39b7f7ca931d6d3be2d6dad377750eb
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2023-04-12 23:33:25 +0530

    hero section done

--- script.js
+++ script.js
@@ -13,4 +13,25 @@ window.onload = function () {
       navbar.classList.remove("scroll"); // Remove "scroll" class from navbar when scrolled back above triggerHeight
     }
   });
+
+  // ********************************************************************************************************************
+
+  const text =
+    "The concentration of carbon dioxide in the atmosphere has increased about 50% since the pre-industrial era, primarily due to human activities."; // Text to be typed
+  const typingText = document.getElementById("typing-text"); // Span element for typing text
+  const typingCursor = document.getElementById("typing-cursor"); // Span element for typing cursor
+
+  let index = 0; // Index to keep track of current character
+
+  function type() {
+    if (index < text.length) {
+      typingText.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, 20); // Delay between each character typing
+    } else {
+      typingCursor.style.display = "none"; // Hide typing cursor when typing is complete
+    }
+  }
+
+  type(); // Call the typing function to start the animation
 };
