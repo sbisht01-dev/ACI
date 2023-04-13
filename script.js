6d72db1445770d4f1183fd4fdca09f8e899d3de5
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2023-04-13 14:32:55 +0530

    CTA added

--- script.js
+++ script.js
@@ -15,23 +15,49 @@ window.onload = function () {
   });
 
   // ********************************************************************************************************************
-
-  const text =
+  let text =
     "The concentration of carbon dioxide in the atmosphere has increased about 50% since the pre-industrial era, primarily due to human activities."; // Text to be typed
-  const typingText = document.getElementById("typing-text"); // Span element for typing text
-  const typingCursor = document.getElementById("typing-cursor"); // Span element for typing cursor
 
-  let index = 0; // Index to keep track of current character
+  function front() {
+    const typingText = document.getElementById("typing-text"); // Span element for typing text
+    const typingCursor = document.getElementById("typing-cursor"); // Span element for typing cursor
 
-  function type() {
-    if (index < text.length) {
-      typingText.textContent += text.charAt(index);
-      index++;
-      setTimeout(type, 20); // Delay between each character typing
-    } else {
-      typingCursor.style.display = "none"; // Hide typing cursor when typing is complete
+    let index = 0; // Index to keep track of current character
+
+    function type() {
+      if (index < text.length) {
+        typingText.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 20); // Delay between each character typing
+      } else {
+        typingCursor.style.display = "none"; // Hide typing cursor when typing is complete
+      }
     }
-  }
 
-  type(); // Call the typing function to start the animation
+    type(); // Call the typing function to start the animation
+  }
+  front();
+
+  let cta = document.querySelector(".CTA");
+  cta.addEventListener("mouseenter", () => {
+    // addition typing class enter + the text
+    console.log();
+    let emission = document.querySelector(".emission");
+    emission.style.fontSize = "1rem ";
+    emission.style.fontWeight = "500";
+    function add() {
+      emission.innerHTML = "Carbon Emission of this website is 4.61gm.";
+      emission.style.padding = "16px";
+    }
+    setTimeout(add, 200);
+  });
+  cta.addEventListener("mouseleave", () => {
+    // removal of the typing class remove + the text
+    let emission = document.querySelector(".emission");
+    emission.innerText = "!";
+    function remove() {
+      emission.style.fontSize = "2rem";
+    }
+    setTimeout(remove, 100);
+  });
 };
